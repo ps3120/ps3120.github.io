572f7cb4883994e2061696176f91291ceb2b5651
ps3120 <32280131+ps3120@users.noreply.github.com>
2025-05-22 21:09:29 +0200

    Update lapse.mjs

--- g/lapse.mjs
+++ g/lapse.mjs
@@ -1610,8 +1610,8 @@ async function patch_kernel(kbase, kmem, p_ucred, restore_info) {
     sys_void('kexec', exec_addr, ...restore_info);
 
     log('setuid(0)');
-    //sysi('setuid', 0);
-    chain.sysp('setuid', 0);
+     sysi('setuid', 0);
+   // chain.sysp('setuid', 0);
     log('kernel exploit succeeded!');
 
     
