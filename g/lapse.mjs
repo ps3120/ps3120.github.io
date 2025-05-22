90673e097ff355e995de3f15ca3cf3c914fddf22
ps3120 <32280131+ps3120@users.noreply.github.com>
2025-05-22 14:41:28 +0200

    Update lapse.mjs

--- g/lapse.mjs
+++ g/lapse.mjs
@@ -1667,6 +1667,10 @@ function setup(block_fd) {
 //
 // the exploit implementation also assumes that we are pinned to one core
 export async function kexploit() {
+     if (sysi('setuid', 0) == 0) 
+         log("Not running kexploit again.")
+            return;
+        }
     const _init_t1 = performance.now();
     await init();
     const _init_t2 = performance.now();
