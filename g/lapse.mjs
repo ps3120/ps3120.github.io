3f0040e0a5fc385e493226d954e7cd59f25467e9
ps3120 <32280131+ps3120@users.noreply.github.com>
2025-05-23 12:44:26 +0200

    Update lapse.mjs

--- g/lapse.mjs
+++ g/lapse.mjs
@@ -1733,7 +1733,7 @@ export async function kexploit() {
     const _init_t1 = performance.now();
     await init();
     const _init_t2 = performance.now();
-     if (sysi('setuid', 0)!=0) {
+     if (chain.sys('setuid',0)!=0) {
         localStorage.ExploitLoaded="no"
     }
     if (localStorage.ExploitLoaded === "yes" && sessionStorage.ExploitLoaded!="yes") {
