a0860e2c8641c0a7833b8c52c811abe992afbc0d
ps3120 <32280131+ps3120@users.noreply.github.com>
2025-05-23 13:12:58 +0200

    Update lapse.mjs

--- g/lapse.mjs
+++ g/lapse.mjs
@@ -1741,7 +1741,8 @@ export async function kexploit() {
     
     if (localStorage.ExploitLoaded === "yes" && sessionStorage.ExploitLoaded!="yes") {
             runBinLoader();
-            return Promise.reject();
+            return;
+            //return Promise.reject();
         }
 
     // fun fact:
@@ -1819,7 +1820,7 @@ export async function kexploit() {
 }
 
 kexploit().then(() => {
-
+ alert("test");
     window.pld_size = new Int(0x26200000, 0x9);
 
     var payload_buffer = chain.sysp('mmap', window.pld_size, 0x300000, 7, 0x41000, -1, 0);
