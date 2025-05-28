f707ac035304970edfca6404f05e237b7c8f4093
ps3120 <32280131+ps3120@users.noreply.github.com>
2025-05-28 15:25:37 +0200

    Update payload.js

--- psfree/payload.js
+++ psfree/payload.js
@@ -1,6 +1,19 @@
 fetch('./payload.bin').then(res => {
     res.arrayBuffer().then(arr => {
-        window.pld = new Uint32Array(arr);
+
+
+          const byteLen = arr.byteLength;
+
+        const padLength = (4 - (byteLen % 4)) % 4;
+    const padding = new Uint8Array(padLength); 
+
+        const alignedBuf = new ArrayBuffer(byteLen + padLength);
+    const tmp8 = new Uint8Array(alignedBuf);
+            tmp8.set(new Uint8Array(arr), 0);
+
+            window.pld = new Uint32Array(alignedBuf);
+
+       // window.pld = new Uint32Array(arr);
 
     })
 })
