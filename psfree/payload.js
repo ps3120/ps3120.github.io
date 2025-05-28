01e338fbcf973ae307e1ffa7ba60e1ae2643814d
ps3120 <32280131+ps3120@users.noreply.github.com>
2025-05-28 15:33:12 +0200

    Update payload.js

--- psfree/payload.js
+++ psfree/payload.js
@@ -2,17 +2,29 @@ fetch('./payload.bin').then(res => {
     res.arrayBuffer().then(arr => {
 
 
-          const byteLen = arr.byteLength;
+  
+    const byteLen   = arr.byteLength;
+  
+    const padLength = (4 - (byteLen % 4)) % 4;
 
-        const padLength = (4 - (byteLen % 4)) % 4;
-    const padding = new Uint8Array(padLength); 
+    console.log("byteLen:", byteLen, "padLength:", padLength);
 
-        const alignedBuf = new ArrayBuffer(byteLen + padLength);
-    const tmp8 = new Uint8Array(alignedBuf);
-            tmp8.set(new Uint8Array(arr), 0);
+  
+    const alignedBuf = new ArrayBuffer(byteLen + padLength);
+    const tmp8       = new Uint8Array(alignedBuf);
 
-            window.pld = new Uint32Array(alignedBuf);
+    
+    tmp8.set(new Uint8Array(arr), 0);
 
+   
+    if (padLength > 0) {
+      const padding = new Uint8Array(padLength); 
+      tmp8.set(padding, byteLen);
+    }
+
+    const totalLen = alignedBuf.byteLength;
+
+    window.pld = new Uint32Array(alignedBuf)
        // window.pld = new Uint32Array(arr);
 
     })
