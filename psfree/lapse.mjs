5e1cf8b03c2e355db6529cff8aeb6c5fd0a5b339
ps3120 <32280131+ps3120@users.noreply.github.com>
2025-05-30 14:50:06 +0200

    Update lapse.mjs

--- psfree/lapse.mjs
+++ psfree/lapse.mjs
@@ -1701,7 +1701,7 @@ export async function kexploit() {
     }
 
     // If setuid is successful, we dont need to run the kexploit again
-    try {
+   /* try {
         if (sysi('setuid', 0) == 0) {
             log("Not running kexploit again.");
             runBinLoader();
@@ -1709,7 +1709,7 @@ export async function kexploit() {
         }
     }
     catch (e) {}
-
+*/
     // fun fact:
     // if the first thing you do since boot is run the web browser, WebKit can
     // use all the cores
