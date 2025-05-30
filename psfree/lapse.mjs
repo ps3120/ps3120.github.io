3c4d755667f69ecbb94c4003abb11909c26abc53
ps3120 <32280131+ps3120@users.noreply.github.com>
2025-05-30 20:57:49 +0200

    Update lapse.mjs

--- psfree/lapse.mjs
+++ psfree/lapse.mjs
@@ -1458,6 +1458,50 @@ export async function kexploit() {
         close(block_fd);
         close(unblock_fd);
     } catch (_) {}
+
+    kexploit().then(() => {
+    const PROT_READ = 1;
+    const PROT_WRITE = 2;
+    const PROT_EXEC = 4;
+
+
+var loader_addr = chain.sysp(
+  'mmap',
+  new Int(0, 0),                         
+  0x1000,                               
+  PROT_READ | PROT_WRITE | PROT_EXEC,    
+  0x41000,                              
+  -1,
+  0
+);
+var tmpStubArray = array_from_address(loader_addr, 1);
+tmpStubArray[0] = 0x00C3E7FF;
+var req = new XMLHttpRequest();
+ req.responseType = "arraybuffer";
+ req.open('GET','payload.bin');
+ req.send();
+ req.onreadystatechange = function () {
+  if (req.readyState == 4) {
+   var PLD = req.response;
+  var payload_buffer = chain.sysp('mmap', new Int(0x26200000, 0x9), 0x300000, 7, 0x41000, -1, 0);
+   var pl = array_from_address(payload_buffer, PLD.byteLength*4);
+   var padding = new Uint8Array(4 - (req.response.byteLength % 4) % 4);
+   var tmp = new Uint8Array(req.response.byteLength + padding.byteLength);
+   tmp.set(new Uint8Array(req.response), 0);
+   tmp.set(padding, req.response.byteLength);
+   var shellcode = new Uint32Array(tmp.buffer);
+   pl.set(shellcode,0);
+   var pthread = malloc(0x10);
+   
+    call_nze(
+        'pthread_create',
+        pthread,
+        0,
+        loader_addr,
+        payload_buffer,
+    );	
+}
+ };
 }
 
  
