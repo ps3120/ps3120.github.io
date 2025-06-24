801d9ffefceaf03c97f7a3ec5ba9912035e9e98d
ps3120 <32280131+ps3120@users.noreply.github.com>
2025-06-24 15:56:48 +0200

    Update ps4.js

--- test/ps4.js
+++ test/ps4.js
@@ -519,6 +519,28 @@ function sprayStringImpl(start, end) {
 }
 
 function go() {
+
+addEventListener('error', event => {
+    const reason = event.error;
+    alert(
+        'Unhandled error\n'
+        + `${reason}\n`
+        + `${reason.sourceURL}:${reason.line}:${reason.column}\n`
+        + `${reason.stack}`
+    );
+    return true;
+});
+
+addEventListener('unhandledrejection', event => {
+    const reason = event.reason;
+    alert(
+        'Unhandled rejection\n'
+        + `${reason}\n`
+        + `${reason.sourceURL}:${reason.line}:${reason.column}\n`
+        + `${reason.stack}`
+    );
+});
+	
 	/* Init spray */
 	sprayHTMLTextArea();
 
