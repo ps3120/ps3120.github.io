dcf042ce2d7d6c76276afe944007f1b413b8f86d
ps3120 <32280131+ps3120@users.noreply.github.com>
2025-06-26 01:09:13 +0200

    Update int64.js

--- w/int64.js
+++ w/int64.js
@@ -13,37 +13,13 @@ function int64(low, hi) {
         this.hi = new_hi;
         this.low = new_lo;
     }
-    this.byteAt = function(i) {
-    
-    if (i < 4) {
-        return (this.low >>> (i * 8)) & 0xff;
-    } else {
-        return (this.hi  >>> ((i - 4) * 8)) & 0xff;
-    }
-}
-    this.add=function(other) {
    
-    if (!(other instanceof int64)) {
-        other = new int64(other.low >>> 0, other.hi >>> 0);
-    }
-    let carry = 0;
-    const res = new Array(8);
-    for (let j = 0; j < 8; j++) {
-        const sum = this.byteAt(j) + other.byteAt(j) + carry;
-        res[j]   = sum & 0xff;
-        carry    = sum >>> 8;
-    }
-   
-    const new_low = (res[0]       )
-                  | (res[1] << 8  )
-                  | (res[2] << 16 )
-                  | (res[3] << 24 );
-    const new_hi  = (res[4]       )
-                  | (res[5] << 8  )
-                  | (res[6] << 16 )
-                  | (res[7] << 24 );
-    return new int64(new_low >>> 0, new_hi >>> 0);
-}
+  this.add = function(other) {
+    let lo = (this.low + other.low) >>> 0;
+    let carry = (lo < this.low) ? 1 : 0;
+    let hi = (this.hi + other.hi + carry) >>> 0;
+    return new int64(lo, hi);
+  };
 
     this.add32 = function (val) {
         var new_lo = (((this.low >>> 0) + val) & 0xFFFFFFFF) >>> 0;
