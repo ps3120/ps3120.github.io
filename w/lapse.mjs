697ecd8e4f562630861afe4bcf9b3407b21bd222
ps3120 <32280131+ps3120@users.noreply.github.com>
2025-06-26 17:41:25 +0200

    Update lapse.mjs

--- w/lapse.mjs
+++ w/lapse.mjs
@@ -152,7 +152,7 @@ const PROT_EXEC = 4;
   await kexploit();
 })();
 */
-function GO() {
+
 
 async function init() {
 try {
@@ -1878,4 +1878,4 @@ fetch('./payload.bin').then(res => res.arrayBuffer()).then(arr => {
 });
 
 })
-}
+ 
