1f768465b349c4aacf48333ed239d37d735004bc
ps3120 <32280131+ps3120@users.noreply.github.com>
2025-06-27 21:45:53 +0200

    Update ps4.js

--- test/ps4.js
+++ test/ps4.js
@@ -46,7 +46,7 @@ var g_input = null;
 
 var guess_htmltextarea_addr = new Int64("0x2031b00d8");
 
-var shared_buf = new ArrayBuffer(0x4000);  
+var shared_buf = new ArrayBuffer(0x1000);  
 
 var master_b = new Uint32Array(shared_buf);
 var slave_b =  new DataView(shared_buf);
